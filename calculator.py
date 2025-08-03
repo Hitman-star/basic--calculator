6425ae60acf7d401f7312d268595a0c6b2e9d1cd
Hitman-star <luisjuma38@gmail.com>
2025-08-03 18:19:18 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,24 @@
+# Basic Calculator Program
+
+# Given Values
+num1 = 27
+num2 = 3
+operation = '+'
+
+if operation == '+':
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Cannot divide by zero.")
+else:
+    print("Error: Invalid operation.")
